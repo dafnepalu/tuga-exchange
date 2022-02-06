0808cbe02c88891191e556356be490b121a55c29
Dafne Palú <paludafne@gmail.com>
2022-02-06 11:43:19 +0100

    Started creating the menus on MóduloPrincipal

--- TugaExchange/MóduloPrincipal/Program.cs
+++ TugaExchange/MóduloPrincipal/Program.cs
@@ -1,19 +1,52 @@
 ﻿// O módulo principal é o responsável por gerir as transações de compra e venda, devendo também permitir a visualização de relatórios de lucros provenientes das comissões.
 
-// Selecione uma opção para entrar
-Console.WriteLine("Selecione uma opção para entrar: (a) Investidor (b) Administrador");
-char option = Convert.ToChar(Console.ReadLine());
+char option;
 
-// Determinar o menu em que entrar
-if (option == 'a' | option == 'A')
+// Selecione uma opção para entrar
+start:
+try
 {
-    Console.WriteLine("Bem-vind@, investidor@");
+    do
+    {
+        Console.WriteLine("Selecione uma opção para entrar: (a) Investidor (b) Administrador");
+        option = Convert.ToChar(Console.ReadLine()); // Tem que ser exatamente um caractere - prever uma exceção 
+    }
+    while (option != 'a' | option != 'A' | option != 'b' | option != 'B');
+
+    // Determinar o menu em que entrar
+    if (option == 'a' | option == 'A')
+    {
+        Console.Clear();
+        Console.WriteLine("Bem-vindo/a, investidor/a");
+        Console.WriteLine("Selecione uma opção:");
+        Console.WriteLine("(1) Depositar");
+        Console.WriteLine("(2) Comprar moeda");
+        Console.WriteLine("(3) Vender moeda");
+        Console.WriteLine("(4) Mostrar portfolio");
+        Console.WriteLine("(5) Mostrar câmbio");
+        Console.WriteLine("(6) Sair");
+    }
+
+    //else if (option == 'b' | option == 'B')
+    else
+    {
+        Console.Clear();
+        Console.WriteLine("Bem-vindo/a, administrador/a");
+        Console.WriteLine("Selecione uma opção:");
+        Console.WriteLine("(1) Adicionar moeda");
+        Console.WriteLine("(2) Remover moeda");
+        Console.WriteLine("(3) Ver relatório de comissões");
+        Console.WriteLine("(4) Sair");
+    }
+    //else
+    //{
+    //    Console.WriteLine("Por favor, introduza uma opção válida: (a) ou (b)");
+    //}
 }
-else if (option == 'b' | option == 'B')
+catch (Exception e)
 {
-    Console.WriteLine("Bem-vind@, administrador@");
+    Console.WriteLine("Você digitou uma opção válida? Tente novamente.");
+    goto start;
 }
-else
-{
-    Console.WriteLine("Por favor, introduza uma opção válida: (a) ou (b)");
-}
+
+
