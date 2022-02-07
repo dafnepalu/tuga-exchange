7301213ba6c9a5a79f89af9521812f2a34e2d08a
Dafne Palú <paludafne@gmail.com>
2022-02-07 13:16:22 +0100

    Made important changes to the menus in MainModule's Program.cs

--- TugaExchange/CryptoQuoteAPI/Coin.cs
+++ TugaExchange/CryptoQuoteAPI/Coin.cs
@@ -2,6 +2,20 @@
 {
     public class Coin
     {
+        // Class properties
+        // Private properties aren't meant to be accessed outside of the Class and need
+        // to be modified using a method
 
+        private string coin;
+        private double marketValue;
+
+        // Class methods
+        // Constructor
+
+        public Coin(string coin)
+        {
+            this.coin = coin;
+            marketValue = 1;
+        }
     }
 }
--- TugaExchange/MainModule/Program.cs
+++ TugaExchange/MainModule/Program.cs
@@ -1,2 +1,130 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static void MainMenu()
+{
+    // Determina o comportamento do menu principal
+    char menuChoice;
+    bool isValid;
+
+    do
+    {
+        Console.WriteLine("Selecione uma opção para entrar:");
+        Console.WriteLine("\n");
+        Console.WriteLine("A - Sou investidor/a");
+        Console.WriteLine("B - Sou administrador/a");
+        Console.WriteLine("\n");
+
+        isValid = Char.TryParse(Console.ReadLine(), out menuChoice);
+        string menuChoiceStr = menuChoice.ToString();
+        string menuChoiceStrUpper = menuChoiceStr.ToUpper();
+
+        if (menuChoice != 'a' & menuChoice != 'A' & menuChoice != 'b' & menuChoice != 'B')
+        {
+            Console.WriteLine("Tem certeza de que inseriu uma opção válida?");
+        }
+        else
+        {
+            Console.WriteLine($"Você escolheu a opção {menuChoiceStrUpper}.");
+        }
+    }
+    while (menuChoice != 'a' & menuChoice != 'A' & menuChoice != 'b' & menuChoice != 'B');
+
+    // Find a way to delay clearing the console for a bit (Task.Delay?)
+    Console.Clear();
+
+    switch (menuChoice)
+    {
+        case 'A':
+            InvestorMenu();
+            break;
+        case 'a':
+            InvestorMenu();
+            break;
+        case 'B':
+            Console.WriteLine("Você selecionou a opção B.");
+            break;
+        case 'b':
+            Console.WriteLine("Você selecionou a opção B.");
+            break;
+    }
+}
+
+static void InvestorMenu()
+{
+    int menuChoice;
+    bool isValid;
+
+    Console.WriteLine("Bem-vindo/a, investidor/a.");
+
+    do
+    {
+        Console.WriteLine("O que deseja fazer?");
+        Console.WriteLine("\n");
+        Console.WriteLine("1 - Depositar");
+        Console.WriteLine("2 - Comprar moeda");
+        Console.WriteLine("3 - Vender moeda");
+        Console.WriteLine("4 - Mostrar portfolio");
+        Console.WriteLine("5 - Mostrar câmbio");
+        Console.WriteLine("6 - Voltar ao menu principal");
+        Console.WriteLine("7 - Sair");
+
+        Console.WriteLine("\n");
+
+        isValid = Int32.TryParse(Console.ReadLine(), out menuChoice);
+    }
+    while (menuChoice != '1' & menuChoice != '2' & menuChoice != '3' & menuChoice != '4' & menuChoice != '5' & menuChoice != '6' & menuChoice != '7');
+
+    Console.Clear();
+
+    switch (menuChoice)
+    {
+        case '1':
+            Console.WriteLine("Você selecionou a opção 1.");
+            break;
+        case '2':
+            Console.WriteLine("Você selecionou a opção 2.");
+            break;
+        case '3':
+            Console.WriteLine("Você selecionou a opção 3.");
+            break;
+        case '4':
+            Console.WriteLine("Você selecionou a opção 4.");
+            break;
+        case '5':
+            Console.WriteLine("Você selecionou a opção 5.");
+            break;
+        case '6':
+            Console.WriteLine("Você selecionou a opção 6.");
+            break;
+        case '7':
+            Console.WriteLine("Você selecionou a opção 7.");
+            break;
+        default:
+            Console.WriteLine("Por favor, selecione uma opção válida.");
+            break;
+    }
+}
+
+// Faixa de boas-vindas
+
+string welcomeText = "TugaExchange - Cripto com confiança";
+string welcomeText50 = welcomeText.PadLeft(65, ' ');
+
+for (int i = 0; i < 48; i++)
+{
+    Console.Write("* ");
+}
+Console.WriteLine("\n");
+Console.WriteLine(welcomeText50);
+Console.WriteLine("\n");
+for (int i = 0; i < 48; i++)
+{
+    Console.Write("* ");
+}
+Console.WriteLine("\n");
+
+// Esperar um pouco até o menu principal aparecer
+// Task.Delay?
+// Limpar a console
+// Console.Clear();
+
+// Chamar o menu principal
+MainMenu();
