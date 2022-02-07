cfc5c785bc5891bfa75477316387dab563bc77f0
Dafne Palú <paludafne@gmail.com>
2022-02-07 09:54:40 +0100

    Changed the structure of the solution. It is now comprised of a Library Project (CryptoQuoteAPI) containing (for now) two Class files (API.cs and Coin.cs), and of a Console App C# (MainModule) containing (for now) a compilable file (Program.cs) and a Class file (Portfolio.cs). MainModule depends on CryptoQuoteAPI

--- TugaExchange/CryptoQuoteAPI/Coin.cs
+++ TugaExchange/CryptoQuoteAPI/Coin.cs
@@ -1,32 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CryptoQuoteAPI
+﻿namespace CryptoQuoteAPI
 {
-    internal class Coin
+    public class Coin
     {
-        // Propriedades de todos os membros da classe
-        // "coin" e "marketValue" estão "private": não quero que sejam acessadas fora da classe, e vou precisar de um método para as modificar
-        private string? coin;
-        private double marketValue;
-
-
-
-        // Construtor
-        private Coin(string coin)
-        {
-            this.coin = coin;
-            // Todas as moedas são criadas com um valor de 1 EUR
-            marketValue = 1;
-        }
-
-        public void AddCoin(string coin)
-        {
-            Console.WriteLine("Hello");
-        }
 
     }
 }
--- TugaExchange/MainModule/Portfolio.cs
+++ TugaExchange/MainModule/Portfolio.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MóduloPrincipal
+namespace MainModule
 {
-    internal class Menu
+    internal class Portfolio
     {
     }
 }
